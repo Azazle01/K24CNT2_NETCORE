ef6fac641023bddbe5775c9f55d4518bd6666011
Azazle Balthazar <nimion19@gmail.com>
2026-08-27 21:49:11 +0700

    NXTLesson02

---
+++ NXTLesson02Theory/Controllers/NXTProductController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NXTLesson02Theory.Models;
+namespace NXTLesson02Theory.Controllers
+{
+    public class NXTProductController : Controller
+    {
+        public IActionResult NXTindex()
+        {
+            ViewBag.name = "Nguyễn Xuân Trường ";
+            ViewData["productNXT"] = "Laptop MSI";
+            TempData["UNI"] = "Trường Đại học Nguyễn Trãi - NTU ";
+            return View();
+        }
+        public IActionResult GetProduct()
+        {
+            NxtProduct NxtProduct = new NxtProduct()
+
+            {
+                ProductID = "2410900081",
+                ProductName = "Nguyễn Xuân Trường",
+                YearRelease = 1979,
+                Price = 99999999999999,
+
+            };
+
+            ViewBag.product = NxtProduct;
+            ViewData["product"] = NxtProduct;
+            return View("Product");
+        }
+    }
+}
