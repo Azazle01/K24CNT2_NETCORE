448fa9b2ea5fecd022bf5fdf00610dac64cb8081
Azazle Balthazar <nimion19@gmail.com>
2026-09-09 17:21:40 +0700

    NXTLesson06View

---
+++ NXTLesson06/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NXTLesson06.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ NXTLesson06/ViewComponents/CategoryViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NXTLesson06.ViewComponents
+{
+    public class CategoryViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(int? n)
+        {
+            var categories = new List<Models.Category>
+            {
+                new Models.Category {CategoryId=1,CategoryName= "Electronic",Isactive=true},
+                new Models.Category {CategoryId=2,CategoryName= "Books",Isactive=true},
+                new Models.Category {CategoryId=3,CategoryName= "Clothing",Isactive=false},
+                new Models.Category {CategoryId=4,CategoryName= "Home & Kitchen ",Isactive=true},
+            };
+            n = n ?? 0;
+            var search = categories.Where(c => c.CategoryId>n).ToList();
+            return View(search);
+        }
+    }
+}
